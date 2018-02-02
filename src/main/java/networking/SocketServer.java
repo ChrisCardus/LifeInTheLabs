9b6c2aee1367aa3b13659e6262daab0ea36077a5
Chris Cardus <cxc357@student.bham.ac.uk>
2018-02-02 19:46:17 +0000

    Created the SocketClient to handle client-side shenanigans. Created Commands to store text commands required for the networking stuff. created ServerThreads to give each new client from SocketServer their own thread. Added to SocketServer the ability to connect to multiple clients.

--- src/main/java/networking/SocketServer.java
+++ src/main/java/networking/SocketServer.java
@@ -1,35 +1,66 @@
 package networking;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * The class SocketServer is for creating and maintaining an open socket that clients can connect to.
  * @author Chris Cardus
  */
 public class SocketServer{
-    // The default port that the server will use, unless a different port is specified by the user.
-    int defaultPort = 49100;
-
+	
+	// Determines if the server is listening for new clients.
+	private boolean listening = true;
+	
     /**
-     * @param port Specifies what port number to use. If none specified, use the default of 49100.
-     * @throws Exception This method throws an exception if there is an error with the network connection.
+     * Starts a socket and listens for new clients.
+     * @param port Specifies what port number to use.
      */
-    public static void main(String args[]){
+    public void startServer(int port) {
+    	this.openSocket(port);
+    }
+    
+    /**
+     * Starts a socket and listens for new clients.
+     * Uses the default port 49100.
+     */
+    public void startServer() {
+    	int defaultPort = 49100;
+    	this.openSocket(defaultPort);
+    }
+    
+    /**
+     * @param port Specifies what port to open the socket on.
+     * @throws Exception This method throws an exception if there is an error with the socket.
+     */
+    private void openSocket(int port){
         // Catch any network errors that might occur.
         try {
-            // If there is one or more arguments, the first argument will be a new port number, so use that to create the server socket. If there are no arguments, use the default port number.
-            if(args.length >= 1) {
-                ServerSocket serverSocket = new ServerSocket(args[1]);
-            } else {
-                ServerSocket serverSocket = new ServerSocket(defaultPort);
-            }
-
-            while (true) {
-                Socket socket = ServerSocket.accept();
+        	// Create the server socket.
+            ServerSocket serverSocket = new ServerSocket(port);
+            
+            // Listen for new clients.
+            while(listening) {
+            	// Create a new thread to deal with each client that tries to connect.
+            	new ServerThread(serverSocket.accept()).start();
             }
 
         } catch (IOException e) {
-            System.out.println("There is an error with the connection: "+e);
+            System.err.println("There is an error with socket: "+e);
         }
+    }
+    
+    /**
+     * Stops new clients from being able to connect to the server.
+     */
+    public void stopListening() {
+    	listening = false;
+    }
+    
+    /**
+     * Allows the server to listen for and connect to new clients.
+     */
+    public void startListening() {
+    	listening = true;
     }
 }
