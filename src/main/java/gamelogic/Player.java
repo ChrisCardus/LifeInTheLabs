e31c7d9e14f4fe87d3b3731454cdea7e1e2278c8
oab630 <oab630@student.bham.ac.uk>
2018-02-06 16:57:02 +0000

    Added gamelogic package

--- src/main/java/gamelogic/Player.java
+++ src/main/java/gamelogic/Player.java
@@ -1,5 +1,23 @@
 package gamelogic;
 
+import graphics.Avatars;
+import graphics.Room;
+
+/**
+ * For storing player data and functionality.
+ * @author Olivia Breen
+ */
+
 public class Player {
+	private int health;
+	private int social;
+	private int education;
+	private int money;
+	private float morality;
+	public Room location;
+	public Avatars avatar;
+	
+	
+	
 
 }
