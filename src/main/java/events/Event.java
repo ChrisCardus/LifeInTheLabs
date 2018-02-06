2c7fd789a79ac99808ee0b4cc7a9cfd63cda694a
oab630 <oab630@student.bham.ac.uk>
2018-02-06 16:10:37 +0000

    Added gamelogic package

--- src/main/java/events/Event.java
+++ src/main/java/events/Event.java
@@ -1,5 +1,8 @@
 package events;
-
+/**
+ * The interface Event is for creating a structure that we can create multiple in game events from.
+ * @author Olivia Breen
+ */
 public interface Event {
 
 }
