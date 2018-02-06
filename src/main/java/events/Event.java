97fd3fa9f50f237d8e3a2fa79bc62f1d35d722eb
oab630 <oab630@student.bham.ac.uk>
2018-02-06 18:00:09 +0000

    Added to Event Class

--- src/main/java/events/Event.java
+++ src/main/java/events/Event.java
@@ -1,8 +1,17 @@
 package events;
+
+import java.util.ArrayList;
+
 /**
  * The interface Event is for creating a structure that we can create multiple in game events from.
  * @author Olivia Breen
  */
 public interface Event {
 
+	public int effects[] = new int[4];
+	public float moralityeffect;
+	
+	
+	
+	
 }
