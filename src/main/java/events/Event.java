94ee7c24eab4d179d086fc876e20c0a4d7999076
oab630 <oab630@student.bham.ac.uk>
2018-02-12 17:27:08 +0000

    Small edits to events

--- src/main/java/events/Event.java
+++ src/main/java/events/Event.java
@@ -1,9 +1,5 @@
 package events;
 
-import java.util.ArrayList;
-
-import gamelogic.Player;
-
 /**
  * The interface Event is for creating a structure that we can create multiple in-game events from.
  * @author Olivia Breen
