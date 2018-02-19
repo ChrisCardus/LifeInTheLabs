97b3c27872fd20ef1d8677530d5f63829bd53c82
Chris Cardus <cxc357@student.bham.ac.uk>
2018-02-19 19:45:34 +0000

    Merged with game_logic

--- src/main/java/gamelogic/Multiplayer.java
+++ src/main/java/gamelogic/Multiplayer.java
@@ -1,26 +1,27 @@
 package gamelogic;
 
 import graphics.Avatars;
+import networking.SocketServer;
 
 public class Multiplayer {
-	private Player[] Players;
-	private int numberOfPlayers = 0;
-	private SocketServer server;
-	
-	public void startServer(int port) {
-		server = new SocketServer(port);
-	}
-	
-	public void addPlayer(String username, Avatars avatar) {
-		Player player = new Player(username, numberOfPlayers, avatar);
-		Players[numberOfPlayers] = player;
-		numberOfPlayers++;
-		if(numberOfPlayers > 9) {
-			server.stopListening();
-		}
-	}
-	
-	public int getLastID() {
-		return (numberOfPlayers - 1);
-	}
+    private Player[] Players;
+    private int numberOfPlayers = 0;
+    private SocketServer server = new SocketServer();
+
+    public void startServer(int port) {
+        server.startServer(port);
+    }
+
+    public void addPlayer(String username, Avatars avatar) {
+        Player player = new Player(username, numberOfPlayers, avatar);
+        Players[numberOfPlayers] = player;
+        numberOfPlayers++;
+        if(numberOfPlayers > 9) {
+            server.stopListening();
+        }
+    }
+
+    public int getLastID() {
+        return (numberOfPlayers - 1);
+    }
 }
