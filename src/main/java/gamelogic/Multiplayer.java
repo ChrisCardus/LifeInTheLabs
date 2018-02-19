9e8d95be40248f8dd878096d601963e51ab8af08
Chris Cardus <cxc357@student.bham.ac.uk>
2018-02-19 19:43:10 +0000

    Merged with networking

--- src/main/java/gamelogic/Multiplayer.java
+++ src/main/java/gamelogic/Multiplayer.java
@@ -1,14 +1,15 @@
 package gamelogic;
 
 import graphics.Avatars;
+import networking.SocketServer;
 
 public class Multiplayer {
 	private Player[] Players;
 	private int numberOfPlayers = 0;
-	private SocketServer server;
+	private SocketServer server = new SocketServer();
 	
 	public void startServer(int port) {
-		server = new SocketServer(port);
+		server.startServer(port);
 	}
 	
 	public void addPlayer(String username, Avatars avatar) {
