161c5573baaf39cb5c88af79f24380e820d51d11
oab630 <oab630@student.bham.ac.uk>
2018-02-20 20:31:39 +0000

    finished location events, modified eventmanager

--- src/main/java/events/EventManager.java
+++ src/main/java/events/EventManager.java
@@ -19,8 +19,8 @@ public class EventManager {
 	boolean LocationEntered(Player player, int location) {
 		int n = random.nextInt(101);
 		if (n < 40) {
-			Object[] event = locationevents.GetEvent(location);
-			RunEvent(event, player);
+			int[][] event = locationevents.GetEvent(location);
+			RunEvent(event);
 			return true;
 		}
 		else {
@@ -29,17 +29,21 @@ public class EventManager {
 	}
 	
 	@SuppressWarnings("unused")
-	void RunEvent(Object[] event, Player player) {
+	void RunEvent(Object[] event) {
 		// Change attributes, signal to UI to display text box
 		// Object[1] = Health, 2 = Social, 3 = Education, 4 = Money, 5 = Morality, 6 = Energy, 7 = Text, 8 = Location
-		int healthchange = (int) event[1];
-		int socialchange = (int) event[2];
-		int educationchange = (int) event[3];
-		int moneychange = (int) event[4];
-		float moralitychange = (float) event[5];
-		int energychange = (int) event[6];
 		String text = (String) event[7];
 		// UI method that takes String as a parameter, splits String into 3 and displays event
+		
+	}
+	void UpdateAttributes(int[][] event, int choice, Player player) {	
+
+		int healthchange = event[choice][1];
+		int socialchange = event[choice][2];
+		int educationchange = event[choice][3];
+		int moneychange = event[choice][4];
+		float moralitychange = event[choice][5];
+		int energychange = event[choice][6];
 		
 		int health = player.getHealth();
 		int social = player.getSocial();
