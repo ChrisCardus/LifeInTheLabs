76ca31b08b7678cba42c40ae5beabc7fed67612d
oab630 <oab630@student.bham.ac.uk>
2018-02-20 22:16:43 +0000

    started Event Strings

--- src/main/java/events/LocationEvents.java
+++ src/main/java/events/LocationEvents.java
@@ -11,6 +11,13 @@ public class LocationEvents {
 	//locations: 1 Lecture, 2 Gym, 3 Home, 4 CS Building, 5 Guild
 	// Object[1] = Health, 2 = Social, 3 = Education, 4 = Money, 5 = Morality, 6 = Energy, 7 = Location
 
+	public String[] EventString1 = {"You arrive at the Lecture only to find it has been cancelled! Do you either use the extra time to go home and take a nap, or go and finish some work in the labs since you're already on campus?", "Nap Time!", "Finish Work.", "The most satisfying nap of your life. You wake up feeling refreshed and ready to take on the world!", "You get some work done, but you're easily distracted when some of your friends come to join. Still, better than no work done!"};
+	public String[] EventString2 = {};
+	public String[] EventString3 = {};
+	public String[] EventString4 = {};
+	public String[] EventString5 = {};
+	
+	
 	public int[][] Event1 = {
 			{0, 10, -5},
 			{0, 0, 5},
@@ -85,7 +92,8 @@ public class LocationEvents {
 		
 	}
 	
-	String GetEventString(int location) {
+	String[] GetEventString(int location) {
+		return null;
 		
 	}
 }
